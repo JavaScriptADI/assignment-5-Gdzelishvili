7dbc962bb885dda7bfcb2b6f15918d79aa653097
Lasha <grdzelishvili.lasha@yahoo.com>
2024-02-26 19:50:12 +0400

    finished task 2

--- task_2.js
+++ task_2.js
@@ -4,3 +4,37 @@
  * "string" -ს და შემდგომ გამოიტანეთ ამ ელემენტის ზომა, მნიშვნელობა და რომელ ინდექსზეა ის
  * "console.log"-ის გამოყენებით
  */
+
+
+let stringArray = ["dodo", "shanxai", "makulatura", "prometeus"]
+let resultObj = [];
+
+
+function smallestWord(myArray){
+    
+    for(let i = 0; i < myArray.length; i++){
+        let obj = { 
+            index: i,
+            lengthy: myArray[i].length,
+            word: myArray[i]
+        }
+        resultObj.push(obj);
+    }
+    console.log(resultObj);
+}
+
+smallestWord(stringArray);
+
+let saveLength = Infinity
+let smallWord = ''
+let indexOfSmallWord = 0
+
+for (let p = 0; p<resultObj.length; p++){
+    if(resultObj[p].lengthy < saveLength ){
+        saveLength = resultObj[p].lengthy;
+        smallWord = resultObj[p].word;
+        indexOfSmallWord = resultObj[p].index;
+    }
+    
+}
+console.log(saveLength, smallWord, indexOfSmallWord);

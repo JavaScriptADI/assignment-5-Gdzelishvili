beb302e3269cac9adbe8bc52a9bda5cd7f5d478a
Lasha <grdzelishvili.lasha@yahoo.com>
2024-02-26 17:06:00 +0400

    finished firs task

--- task_1.js
+++ task_1.js
@@ -2,3 +2,17 @@
  * დაწერეთ კოდი, რომელიც მომხმარებელს "prompt" ფუნქციის გამოყენებით
  * შემოატანინებს 10 სახელს და შეინახეთ ეს მნიშვნელობები "array" -ში.
  */
+
+
+let name1 = prompt("enter name 1");
+let name2 = prompt("enter name 2");
+let name3 = prompt("enter name 3");
+let name4 = prompt("enter name 4");
+let name5 = prompt("enter name 5");
+let name6 = prompt("enter name 6");
+let name7 = prompt("enter name 7");
+let name8 = prompt("enter name 8");
+let name9 = prompt("enter name 9");
+let name10 = prompt("enter name 10");
+
+let nameArray = [name1, name2, name3, name4, name5, name6, name7, name8, name9, name10];

67826d9ae73f5cd0f5c25586333392047bae4795
Lasha <grdzelishvili.lasha@yahoo.com>
2024-02-28 16:05:48 +0400

    finished 5th task

--- task_5.js
+++ task_5.js
@@ -1,3 +1,19 @@
-/**
- * დაწერეთ ფუნქცია, რომელიც "string" -ების "array" -ში დათვლის საშუალოდ რა ზომის "string" -ებია
- */
+
+//  * დაწერეთ ფუნქცია, რომელიც "string" -ების "array" -ში დათვლის საშუალოდ რა ზომის "string" -ებია
+//  */
+
+let stringArray1 = ["dodo", "shanxai", "makulatura", "prometeus"];
+let sumCount = 0
+
+
+function counteAvrage(strings){
+    
+    for(i = 0; i<strings.length; i++){
+        sumCount += strings[i].length
+            
+    }
+    return sumCount/strings.length
+};
+
+
+console.log(counteAvrage(stringArray1));

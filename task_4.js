e2717cde464321da32ce13b1ec373f17cdcdd575
Lasha <grdzelishvili.lasha@yahoo.com>
2024-02-28 15:55:41 +0400

    finished 4th task

--- task_4.js
+++ task_4.js
@@ -14,3 +14,22 @@
  * თითოეული ელემენტისთვის დააგენერირეთ ეს HTML კოდი და დაწერეთ "console.log" -ში მთლიანი აგებული სტრინგი.
  * ანუ ყველა პროდუქტის HTML -ი ერთ დიდ "string" -ში უნდა შეინახოთ!
  */
+
+
+let productName = ['vashli', "msxali", 'atami'];
+
+let productPrice = ['1', '1,25', '3'];
+
+let sumString = "" 
+
+function pricer(names, prices){
+    for(i=0; i<names.length; i++){
+        sumString += `<div>
+        <p>${names}</p>
+        <p>${prices}</p>
+   </div>`
+    }
+}
+
+pricer(productName, productPrice);
+console.log(sumString);

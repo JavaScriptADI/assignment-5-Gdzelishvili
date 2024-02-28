3b3ff673ee8f168c7c1458b4f89b436797cb9743
Lasha <grdzelishvili.lasha@yahoo.com>
2024-02-28 15:39:38 +0400

    ყველა ვარიანტში 1ს მაჩვენებს პასუხს ეს ვერ გავიგე რატო

--- task_3.js
+++ task_3.js
@@ -12,3 +12,33 @@
  * კითხვა:
  * აქვს თუ არა თქვენ მიერ დაწერილი ფუნქციას side effect -ი?
  */
+
+
+let strings = ["hahaha", "world", "open", "hans solo", "awesome"];
+let count = 0;
+
+function UppercaseStrings(arr) {
+    
+    for (let i = 0; i < arr.length; i++) {
+        let string = arr[i];
+        for (let j = 0; j < string.length; j++) {
+            if (string[j] === string[j].toUpperCase()) {
+                count++;
+                break; 
+            }
+        }
+    }
+    return count;
+}
+
+
+let result = UppercaseStrings(strings);
+
+if (result === 0) {
+    console.log("There are no uppercase strings in the array.");
+} else {
+    console.log("How many uppercase strings are in array?", result);
+}
+
+
+// საიდ ეფექტები ისაა, რომ  ვერანაირად 0 ვერ ვაჩვენებინე სულ რომ არც იყოს დიდი ასო სტრინგში
